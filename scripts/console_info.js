886df5768fde7f83b4dbcc486ec429b6c64a1970
Nilay Technological <79686507+nilay-technological@users.noreply.github.com>
2021-05-07 11:10:09 +0530

    Update console_info.js

--- scripts/console_info.js
+++ scripts/console_info.js
@@ -1,5 +1,3 @@
+console.log("You are viewing Nilay Github official website. This website is created my Nilay Technological. Visit https://nilaytechnological.blogspot.com or tweet us @nilay_tech for more info");
 console.log("Official url for this website is https://nilay-technological.github.io");
 console.log("The shorten url for this website is https://bit.ly/nilaygithub");
-console.log("Important links if you have any query");
-console.log("https://nilaytechnological.blogspot.com");
-console.log("https://twitter.com/nilay_tech");
